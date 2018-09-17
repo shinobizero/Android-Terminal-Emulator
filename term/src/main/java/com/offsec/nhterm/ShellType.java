bc91f63465aaa9ffda46ed71854f1f7d8ca716a0
shinobizero <shinobizero@gmx.com>
2018-09-17 11:46:27 +1000

    Changed default shells to bash

--- term/src/main/java/com/offsec/nhterm/ShellType.java
+++ term/src/main/java/com/offsec/nhterm/ShellType.java
@@ -10,8 +10,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 class ShellType {
-    static final String ANDROID_SHELL =  whichCMD("sh") + " -";
-    static final String ANDROID_SU_SHELL = whichCMD("su");
+    static final String ANDROID_SHELL =  whichCMD("/system/xbin/bash") + " -";
+    static final String ANDROID_SU_SHELL = whichCMD("su") + "-c /system/xbin/bash";
     static final String KALI_SHELL = whichCMD("su") + " -c /data/data/com.offsec.nethunter/files/scripts/bootkali";
     static final String KALI_LOGIN_SHELL = whichCMD("su") +" -c /data/data/com.offsec.nethunter/files/scripts/bootkali_login";
 
